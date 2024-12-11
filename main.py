5a1165feb7321364b880690a4b17e0db5de38264
originalmastarex <maximilian.starke@web.de>
2024-12-11 15:34:04 +0100

    variabeln

--- main.py
+++ main.py
@@ -1,5 +1,48 @@
 #alt + z
 
-print("Gaida")
+#import pygame
 
-print("33")
+#Variabeln
+
+lama_url = "https://212.132.112.15:11434/api/chat"
+
+lama_model = "llama3.1:8b-instruct-q5.K_M"
+
+prompt = "p"
+
+prompt__sp = "sp"
+
+prompt_bew = "bew"
+
+prompt1 = "1"
+
+prompt2 = "2"
+
+prompt3 = "3"
+
+prompt4 = "4"
+
+antwort = "a"
+
+data = {
+    "model":lama_model,
+    "messages":[
+        {
+            "role":"system",
+            "content":prompt,
+        }
+    ],
+    "stream":False,
+}
+
+
+
+
+
+def main():
+    d
+
+
+
+
+main()
